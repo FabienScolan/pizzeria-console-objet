1da1f1425e4c058889a0cfa9f380bb2386c5a933
Fabien SCOLAN <fabien.scolan22@gmail.com>
2018-03-21 12:13:19 +0100

    Fin TP4

---
+++ src/vue/AjouterPizzaService.java
@@ -0,0 +1,25 @@
+package vue;
+
+import java.util.Scanner;
+
+import fr.pizzeria.model.Pizza;
+import fr.pizzeria.model.PizzaMemDao;
+
+public class AjouterPizzaService extends MenuService {
+
+	@Override
+	public void executeUC(PizzaMemDao pizzaDao, Scanner questionUser) {
+		// TODO Auto-generated method stub
+		System.out.println("\nAjout d'une nouvelle pizza");
+		System.out.println("Veuillez saisir le code : ");
+		String nvCode = questionUser.next();
+		System.out.println("Veuillez saisir le nom : ");
+		String nvNom = questionUser.next();
+		System.out.println("Veuillez saisir le prix : ");
+		double nvPrix = Double.parseDouble(questionUser.next());
+		Pizza nvPizza = new Pizza(nvCode, nvNom, nvPrix);
+		pizzaDao.saveNewPizza(nvPizza);
+	}
+	
+
+}
---
+++ src/vue/ModifierPizzaService.java
@@ -0,0 +1,29 @@
+package vue;
+
+import java.util.Scanner;
+
+import fr.pizzeria.model.Pizza;
+import fr.pizzeria.model.PizzaMemDao;
+
+public class ModifierPizzaService extends MenuService {
+
+	@Override
+	public void executeUC(PizzaMemDao pizzaDao, Scanner questionUser) {
+		// TODO Auto-generated method stub
+		System.out.println("\nMise à jour de la pizza");
+		for (Pizza pizza : pizzaDao.findAllPizzas()) {
+			if (pizza != null)
+				System.out.println(pizza.toString());
+		}
+		System.out.println("Veuillez saisir le code de la pizza à modifier : ");
+		String editCode = questionUser.next();
+		System.out.println("Veuillez saisir le nouveau nom : ");
+		String editNom = questionUser.next();
+		System.out.println("Veuillez saisir le nouveau prix : ");
+		double editPrix = Double.parseDouble(questionUser.next());
+		Pizza editPizza = new Pizza(editCode, editNom, editPrix);
+		pizzaDao.updatePizza(editCode, editPizza);
+	}
+
+
+}
